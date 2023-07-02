4c65796d6d575083f83f09e760b5405437b6d1b3
HappyEnciso <encisohappy@gmail.com>
2023-07-02 12:25:52 +0800

    adjusted stuff

---
+++ SIS_MODEL/FacultyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIS_MODEL
+{
+    internal class FacultyInfo
+    {
+        public string facultyNumber { get; set; }
+        public string facultyName { get; set; }
+        public string subjectHeld { get; set; }
+
+        public string email { get; set; }
+    }
+}
