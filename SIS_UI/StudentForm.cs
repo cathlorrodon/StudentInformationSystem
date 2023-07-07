aee3df03cbd7a9624d46a570302d222f6d7fa012
Stephen Perez <perezstephenmathew360@gmail.com>
2023-07-08 06:47:40 +0800

    Schedule Module for Students

--- SIS_UI/StudentForm.cs
+++ SIS_UI/StudentForm.cs
@@ -114,7 +114,57 @@ namespace SIS_UI
             Console.WriteLine("\nOn Progress of Rizon's Group\n");
         }
 
+        public static int scheduleSystem()
+        {
+            Console.WriteLine("\nPlease Choose an Action:");
+            Console.WriteLine("[1] Monday");
+            Console.WriteLine("[2] Tuesday");
+            Console.WriteLine("[3] Wednesday");
+            Console.WriteLine("[4] Thursday");
+            Console.WriteLine("[5] Friday");
+            Console.WriteLine("[6] Saturday");
+            Console.Write("Input: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
 
+        public static void scheduleMonday()
+        {
+            Console.WriteLine("No schedule");
+        }
+
+        public static void scheduleTuesday()
+        {
+            Console.WriteLine("\n1. Information Management | 8:00AM to 12:00PM");
+            Console.WriteLine("2. People's Earth & Ecosystem | 2:00PM to 5:00PM");
+            Console.WriteLine("3. Information Management | 5:15PM to 7:00PM");
+        }
+
+        public static void scheduleWednesday()
+        {
+            Console.WriteLine("\nNo schedule");
+            //return Console.ReadLine();
+        }
+
+        public static void scheduleThursday()
+        {
+            Console.WriteLine("\n1. Network Administration | 8:00AM to 1:00PM");
+            Console.WriteLine("2. Physical Education | 4:00PM to 6:00PM");
+            Console.WriteLine("3. Quantitative Method | 6:00PM to 9:00PM");
+            //return Console.ReadLine();
+        }
+
+        public static void scheduleFriday()
+        {
+            Console.WriteLine("\nNo schedule");
+            //return Console.ReadLine();
+        }
+
+        public static void scheduleSaturday()
+        {
+            Console.WriteLine("\n1. Integrative Programming | 8:00AM to 12:30PM");
+            Console.WriteLine("2. Object Oriented Programming | 2:00PM to 8:00PM");
+            //return Console.ReadLine();
+        }
 
     }
 }
