14016432fcc0fb3d18164b36b1b02d0862c7c01c
Catherine Lorraine Odon <116412390+cathlorrodon@users.noreply.github.com>
2023-07-13 22:08:26 +0800

    Update FacultyForm.cs

--- SIS_UI/FacultyForm.cs
+++ SIS_UI/FacultyForm.cs
@@ -35,6 +35,32 @@ namespace SIS_UI
         {
             Console.WriteLine("\nOn Progress of Rizon's Group\n");
         }
+
+        public static void scheduleSystem(string facultyNumber)
+        {
+            Dictionary<string, string> facultySchedule = new Dictionary<string, string>()
+    {
+        { "FA0001BN2016", "Integrative Programming and Technologies 1: Saturday, 8:00 AM - 12:30 PM" },
+        { "FA0002BN2016", "Human Computer Interaction: Tuesday, 5:15 PM - 7:00 PM" },
+        { "FA0003BN2016", "People and Earth's Ecosystem: Tuesday, 2:00 PM - 5:00 PM" },
+        { "FA0004BN2016", "Quantitative Methods with Modeling and Simulation: Thursday, 6:00 PM - 9:00 PM" },
+        { "FA0005BN2016", "Information Management: Tuesday, 8:00 AM - 12:00 PM" },
+        { "FA0006BN2016", "Object Oriented Programming: Saturday, 2:00 PM - 7:00 PM" },
+        { "FA0007BN2016", "Team Sports: Thursday, 4:00 PM - 6:00 PM" }
+    };
+
+
+            string schedule;
+            if (facultySchedule.TryGetValue(facultyNumber, out schedule))
+            {
+                Console.WriteLine($"\nSubject: {schedule}");
+            }
+            else
+            {
+                Console.WriteLine("\nNo schedule available for the selected faculty number.");
+            }
+        }
+
     }
 }
 
